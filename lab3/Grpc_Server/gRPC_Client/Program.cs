b53ee43feeefb39820703d65493a44feb42ee497
Nlk1ta <shmidt.n1kita@yandex.ru>
2023-12-12 23:04:07 +0700

    laba3
    rpc

---
+++ lab3/Grpc_Server/gRPC_Client/Program.cs
@@ -0,0 +1,34 @@
+﻿using Grpc.Core;
+using Grpc.Net.Client;
+using Grpc_Server;
+
+var channel = GrpcChannel.ForAddress("https://localhost:7192");
+var client = new Greeter.GreeterClient(channel);
+
+var reply = await client.SayHelloAsync(
+    new HelloRequest { Name = ".Net lab3 is working..." });
+var call = client.SayHelloStream();
+
+Console.WriteLine("From server: " + reply.Message);
+
+var readTask = Task.Run(async () =>
+{
+    await foreach (var response in call.ResponseStream.ReadAllAsync())
+    {
+        Console.WriteLine(response.Message);
+    }
+});
+
+while (true)
+{
+    var result = Console.ReadLine();
+    if(string.IsNullOrWhiteSpace(result))
+    {
+        break;
+    }
+    await call.RequestStream.WriteAsync(new HelloRequest() { Name = result });
+}
+
+await call.RequestStream.CompleteAsync();
+await readTask;
+
